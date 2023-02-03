947ae21e668f3bff01fa5139e7be56988a517373
GhostWing17 <ghostwing17@gmail.com>
2023-02-03 10:41:04 +0900

    Initial commit

---
+++ app/src/main/java/com/example/textwatcher/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.textwatcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // 유저가 텍스트를 입력할때마다
+                // 텍스트뷰에 똑같이 입력한 문자를 표시
+
+                String text = editText.getText().toString();
+                textView.setText(text);
+
+            }
+        });
+    }
+}
